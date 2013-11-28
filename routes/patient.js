4a6c6254bf4510d11fd7be5c325042a3a3537df5
cyril <cyril.resseguier@gmail.com>
2013-11-28 22:00:27 +0100

    NRRD elaboration 1/2

--- routes/patient.js
+++ routes/patient.js
@@ -2,13 +2,16 @@
 /*
  * GET patient viewer.
  */
+
  
 exports.viewer = function(req, res){
     
     var libxmljs = require("libxmljs");//npm install libxmljs
     var fs = require("fs");//pour lire un fichier
 
+
     var xml = fs.readFileSync("public/temp/test.xml", "utf-8");
+
     var xmlDoc = libxmljs.parseXmlString(xml);
 
     // xpath queries : first node with tag 'element' and attribute 'name' as 'PatientName'
@@ -47,25 +50,61 @@ exports.viewer = function(req, res){
  * POST patient files.
  */
 
+
 exports.upload = function(req, res){
-console.log(req.files);
+    //console.log(req.files);
     var file = req.files.file;
     var sys = require('sys')
     var exec = require('child_process').exec;
+    var fs = require("fs");
  
-    console.log('Temp file path: ' +  file.path);
-    console.log('Original file name: ' + file.name);
+    //console.log('Temp file path: ' +  file.path);
+    //console.log('Original file name: ' + file.name);
     
+    var str = file.name.split(".");
 
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec("dcm2xml "+ file.path +" public/temp/test.xml", puts);
+    if (str[(str.length)-1]=='dcm'){
+        function puts(error, stdout, stderr) { sys.puts(stdout)}
+        exec("dcm2xml "+ file.path +" public/temp/test.xml", puts);
 
-    // RAW file in "try" folder to using binary data
-    exec("dcmdump " +file.path+" +W public/temp",puts);
+        // RAW file in "try" folder to using binary data
+        exec("dcmdump " +file.path+" +W public/temp",puts);
+        res.redirect('viewer');
+        }
 
 
-    //exec("ls", puts);
-    res.redirect('viewer');
+    else if (str[(str.length)-1]=='zip'){
+        exec("unzip " +file.path+ " -d public/temp");
+        //exec("unzip -l "+file.path+" > public/temp/list.txt");
+
+        var callbackzip=function (error, response, body) {
+            fs.readFile("public/temp/list.txt",'utf-8', function (err, contenu) {
+                if (err) throw err;
+        
+
+                var tab = contenu.split(".dcm\n");
+
+                function executiondcm(tab){
+                    console.log("a"+tab[0]+"b");
+                    function puts(error, stdout, stderr) { sys.puts(stdout)}
+                    exec("dcm2xml "+ tab[0] +".dcm public/temp/test.xml",puts);
+                    exec("dcmdump "+ tab[0] +".dcm +W public/temp",puts);
+                    res.redirect('viewer');
+                    };
+
+                executiondcm (tab);
+                });
+        };
+
+        //fonction non robuste a debugger
+        exec("find public/temp/CT -name *.dcm | sort -d > public/temp/list.txt",callbackzip);
+        //exec("head -n 1 public/temp/list.txt");
+        
+
+        //exec("dcm2xml "+ char +" public/temp/test.xml", puts);
+        //exec("dcmdump " +char+" +W public/temp",puts);
+        }
+
 };
 
 
